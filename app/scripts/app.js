306222b213c74d5af430e44581f8f6dbb1f0d15b
bonbon0202 <bonbon0202@gmail.com>
2022-04-15 16:07:29 +0900

    Add: initial settings

---
+++ app/scripts/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular
+  .module('calendar2App', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
